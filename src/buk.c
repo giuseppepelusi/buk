6cd364a29ef09cf296e16fb66a20d188cd6fa9a8
Giuseppe Pelusi <giuseppepelusi2004@gmail.com>
2024-10-30 17:19:07 +0100

    Add help

--- src/buk.c
+++ src/buk.c
@@ -1,8 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-// #include "help/help.h"
+#include <stdio.h>		// For printf, fprintf, perror
+#include <stdlib.h>		// For EXIT_SUCCESS, EXIT_FAILURE
+#include <string.h>		// For strcmp
+#include "globals.h"
+#include "help/help.h"
 #include "init/init.h"
+// #include "config/config.h"
 // #include "save/save.h"
 // #include "delete/delete.h"
 // #include "restore/restore.h"
@@ -13,32 +15,62 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 	{
-		// help();
+		help();
 		return EXIT_FAILURE;
 	}
 	else if (strcmp(option, "help") == 0)
 	{
-		// help();
+		help();
 	}
 	else if (strcmp(option, "init") == 0)
 	{
+		if (argc > 2)
+		{
+			help_init();
+			return EXIT_FAILURE;
+		}
 		init();
+	}
+	else if (strcmp(option, "config") == 0)
+	{
+		if (argc != 3)
+		{
+			help_config();
+			return EXIT_FAILURE;
+		}
+		printf("config_test");
+		// config();
 	}
 	else if (strcmp(option, "save") == 0)
 	{
+		if (argc < 3)
+		{
+			help_save();
+			return EXIT_FAILURE;
+		}
 		// save();
 	}
 	else if (strcmp(option, "delete") == 0)
 	{
+		if (argc < 3)
+		{
+			help_delete();
+			return EXIT_FAILURE;
+		}
 		// delete();
 	}
 	else if (strcmp(option, "restore") == 0)
 	{
+		if (argc < 3)
+		{
+			help_restore();
+			return EXIT_FAILURE;
+		}
 		// restore();
 	}
 	else
 	{
-		fprintf(stderr, "Unknown command: \"%s\", use \"buk help\" for the list of commands.\n", argv[1]);
+		fprintf(stderr, "%s: \'%s\' is not a %s command. See '%s help'.\n", NAME, argv[1], NAME, NAME);
 		return EXIT_FAILURE;
 	}
 
---
+++ src/help/help.c
@@ -0,0 +1,50 @@
+#include <stdio.h>		// For printf, fprintf, perror
+#include "help.h"
+#include "../globals.h"
+
+void help()
+{
+	printf("usage: %s <command> [<args>]\n\n", NAME);
+	printf("These are the available %s commands:\n", NAME);
+	printf("  help:\t\tShow the help page\n");
+	printf("  init:\t\tInitialize the current directory\n");
+	printf("  config:\tConfigure the path of the backup directory\n");
+	printf("  save:\t\tSave the passed files or directories to the backup directory\n");
+	printf("  delete:\tDelete the passed files or directories from the backup directory\n");
+	printf("  restore:\tRestore the passed files or directories from the backup\n\t\tdirectory to the local directory\n");
+}
+
+void help_init()
+{
+	printf("%s: malformed expression\n\n", NAME);
+	printf("usage: %s init\n", NAME);
+	printf("Use \"%s help\" for additional information\n", NAME);
+}
+
+void help_config()
+{
+	printf("%s: malformed expression\n\n", NAME);
+	printf("usage: %s config <path>\n", NAME);
+	printf("Use \"%s help\" for additional information\n", NAME);
+}
+
+void help_save()
+{
+	printf("%s: malformed expression\n\n", NAME);
+	printf("usage: %s save <file|dir> [<file|dir>...]\n", NAME);
+	printf("Use \"%s help\" for additional information\n", NAME);
+}
+
+void help_delete()
+{
+	printf("%s: malformed expression\n\n", NAME);
+	printf("usage: %s delete <file|dir> [<file|dir>...]\n", NAME);
+	printf("Use \"%s help\" for additional information\n", NAME);
+}
+
+void help_restore()
+{
+	printf("%s: malformed expression\n\n", NAME);
+	printf("usage: %s restore <file|dir> [<file|dir>...]\n", NAME);
+	printf("Use \"%s help\" for additional information\n", NAME);
+}
