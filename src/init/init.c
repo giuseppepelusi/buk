af6d457b29f117fe49199be2f242b76444caee79
Giuseppe Pelusi <giuseppepelusi2004@gmail.com>
2024-10-31 12:00:49 +0100

    Fix init.c

--- src/init/init.c
+++ src/init/init.c
@@ -6,13 +6,15 @@
 
 int init()
 {
-	const char *home_dir = get_home_directory();
+	char *home_dir = get_home_directory();
 	char *current_working_dir = get_current_working_directory();
+	char *default_backup_path = (char*)malloc(1000);
+	snprintf(default_backup_path, 1000 , "%s/%s", home_dir, DEFAULT_BACKUP_DIR);
 
 	if (create_folder() == EXIT_SUCCESS)
 	{
-		write_config(home_dir);
-		printf("%s: Initialized buk repository in %s/.%s/\n", NAME, current_working_dir, NAME);
+		write_config(default_backup_path);
+		printf("%s: Initialized %s repository in %s/.%s/\n", NAME, NAME, current_working_dir, NAME);
 		free(current_working_dir);
 		return EXIT_SUCCESS;
 	}
--- src/utils/globals.h
+++ src/utils/globals.h
@@ -3,7 +3,7 @@
 
 #define NAME "buk"
 #define DIR_NAME "." NAME
-#define CONFIG_PATH "." NAME "/config"
+#define CONFIG_PATH "/config"
 #define DEFAULT_BACKUP_DIR "." NAME "_backups"
 
 #endif // GLOBALS_H
