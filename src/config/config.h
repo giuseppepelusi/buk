a020a2ddd05f18a56ad078656334bdd47bcd7ac6
Giuseppe Pelusi <giuseppepelusi2004@gmail.com>
2025-10-01 21:57:21 +0200

    Now config check if path is valid before saving it

--- src/config/config.h
+++ src/config/config.h
@@ -2,5 +2,8 @@
 #define CONFIG_H
 
 int config(char *argv[]);
+int is_path_in_safe_location(const char *path);
+char* get_first_existing_parent(const char *path);
+int validate_and_resolve_path(const char *input_path, char *resolved_path);
 
 #endif // CONFIG_H
