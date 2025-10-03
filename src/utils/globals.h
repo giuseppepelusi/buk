17550f3f6936b3584c84658c7eb9741b7abb4a60
Giuseppe Pelusi <giuseppepelusi2004@gmail.com>
2025-10-03 20:34:50 +0200

    Updated version to 0.9.0-beta

--- src/utils/globals.h
+++ src/utils/globals.h
@@ -5,6 +5,6 @@
 #define DIR_NAME "." NAME
 #define CONFIG_FILE "config"
 #define DEFAULT_BACKUP_DIR "." NAME "_backups"
-#define VERSION "0.6.0-alpha"
+#define VERSION "0.9.0-beta"
 
 #endif // GLOBALS_H
